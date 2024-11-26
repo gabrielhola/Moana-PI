4440721dd7fde454bae22ff843f10b8006e75aa3
gabrielhola <gabriel.holanda@sptech.school>
2024-11-26 13:15:24 -0300

    Exclusão de arquivos desnecessários

--- Site/app.js
+++ Site/app.js
@@ -22,6 +22,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var votacaoRouter = require("./src/routes/votacao");
+// var resultadoRouter = require("./src/routes/resultado");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +37,7 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/votacao", votacaoRouter);
+// app.use("/resultado", resultadoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
