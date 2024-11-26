b4ea7ddfa8dbe0b092c29d9021e38f043294a293
gabrielhola <gabriel.holanda@sptech.school>
2024-11-25 23:57:01 -0300

    Finalização Banco de Dados

--- BANCO DE DADOS/ScriptBD_Moana.sql
+++ BANCO DE DADOS/ScriptBD_Moana.sql
@@ -6,5 +6,42 @@ CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(254),
-	senha CHAR(50)
+	senha CHAR(50),
+    fkPraia int,
+    constraint fkPraiaUsuario foreign key (fkPraia)
+    references praia(idPraia)
 );
+
+select * from usuario;
+
+truncate usuario;
+
+CREATE TABLE praia (
+idPraia int primary key auto_increment,
+nome varchar(45),
+qtdVotos int
+);
+
+
+select * from votacao;
+
+
+select nome, qtdVotos from praia
+order by qtdVotos desc;
+
+insert into praia values
+(default, 'Bora Bora - Polinésia Francesa', 0),
+(default, 'Praia da Conceição - Fernando de Noronha', 0),
+(default, 'Praia Preta - São Sebastião', 0),
+(default, 'Praia do Farol - Arraial do Cabo', 0),
+(default, 'Baía dos Porcos - Fernando de Noronha', 0);
+
+update praia
+set qtdVotos = qtdVotos + 1
+where idPraia = 5;
+
+select nome, qtdVotos from praia
+order by qtdVotos desc;
+
+
+
