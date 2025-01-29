fb06cc155e2c6ddbe5b44288bbee234a6e4cff4c
gabrielhola <gabriel.holanda@sptech.school>
2025-01-29 15:35:10 -0300

    Atualização do Banco de Dados e credenciais 29/01/25

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -42,6 +42,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var visitouPraia = req.body.visitouPraiaServer
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -50,10 +51,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (visitouPraia == undefined) {
+        res.status(400).send("Questionário incompleto!")
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, visitouPraia)
             .then(
                 function (resultado) {
                     res.json(resultado);
